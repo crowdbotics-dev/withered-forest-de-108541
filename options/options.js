e9d1d3b7228084d463bb22ed0f281b9452210052
Crowdbotics <engineering@crowdbotics.com>
2023-08-09 11:49:21 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "withered-forest-de-108541",
+  url: "https://withered-forest-de-108541.botics.co",
+  api: "https://withered-forest-de-108541.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
